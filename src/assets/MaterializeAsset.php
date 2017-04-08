58d6febd12a584e6821be7cc30d4986d7d3eda5b
Philip Verbist <philip.verbist@gmail.com>
2017-04-08 17:48:28 +0200

    correct materialize asset

--- src/assets/MaterializeAsset.php
+++ src/assets/MaterializeAsset.php
@@ -6,6 +6,17 @@ use yii\web\AssetBundle;
 
 class MaterializeAsset extends AssetBundle {
 
+    public $sourcePath = '@bower/materialize/dist';
     
-
+    public $css = [
+        'css/materialize.min.css',
+    ];
+    
+    public $js = [
+        'js/materialize.min.js',
+    ];
+    
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 }
